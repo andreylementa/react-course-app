44160018ad9f493b8093c2308f099cc365d9c469
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-07-20 14:50:28 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import CostItem from "./components/CostItem";
+
 function App() {
+  const costs = [
+    {
+      date: Date.now().toString(),
+      description: "Холодильник",
+      amount: 999.99,
+    },
+    {
+      date: Date.now().toString(),
+      description: "MacBook",
+      amount: 1254.72,
+    },
+    {
+      date: Date.now().toString(),
+      description: "Джинсы",
+      amount: 49.99,
+    },
+  ];
   return (
     <div>
       <h1>Начнем изучение React</h1>
+
+      <CostItem
+        date={costs[0].date}
+        description={costs[0].description}
+        amount={costs[0].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[2].date}
+        description={costs[2].description}
+        amount={costs[2].amount}
+      ></CostItem>
     </div>
   );
 }
