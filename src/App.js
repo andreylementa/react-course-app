cd985fa20210e7d380744619ee3dfc854ea852c2
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-07-22 11:52:00 +0300

    Использование списков с состоянием

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import Costs from "./components/Costs/Costs";
 import NewCost from "./components/NewCost/NewCost";
+import React, { useState } from "react";
+
+const INITIAL_COSTS = [
+  {
+    id: "c1",
+    date: new Date(2021, 2, 12),
+    description: "Холодильник",
+    amount: 999.99,
+  },
+  {
+    id: "c2",
+    date: new Date(2021, 2, 12),
+    description: "MacBook",
+    amount: 1254.72,
+  },
+  {
+    id: "c3",
+    date: new Date(2021, 2, 12),
+    description: "Джинсы",
+    amount: 49.994,
+  },
+];
 
 function App() {
-  const costs = [
-    {
-      id: "c1",
-      date: new Date(2021, 2, 12),
-      description: "Холодильник",
-      amount: 999.99,
-    },
-    {
-      id: "c2",
-      date: new Date(2021, 2, 12),
-      description: "MacBook",
-      amount: 1254.72,
-    },
-    {
-      id: "c3",
-      date: new Date(2021, 2, 12),
-      description: "Джинсы",
-      amount: 49.994,
-    },
-  ];
-
+  const [costs, setCosts] = useState(INITIAL_COSTS);
   const addCostHandler = (cost) => {
-    console.log(cost);
+    setCosts((prevCosts) => {
+      return [cost, ...prevCosts];
+    });
   };
 
   return (
