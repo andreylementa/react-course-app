088863f6e90915af82707e688e75f04fa24d61ce
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-07-22 12:18:50 +0300

    task 3 отображение списка по фильтру года

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -11,12 +11,17 @@ const Costs = (props) => {
     setSelectedYear(year);
   };
 
+  const filteredCosts = props.costs.filter(
+    (cost) => cost.date.getFullYear().toString() === selectedYear
+  );
+
   return (
     <div>
       <Card className="costs">
         <CostsFilter year={selectedYear} onChangeYear={changeYear} />
-        {props.costs.map((cost) => (
+        {filteredCosts.map((cost) => (
           <CostItem
+            key={cost.id}
             date={cost.date}
             description={cost.description}
             amount={cost.amount}
