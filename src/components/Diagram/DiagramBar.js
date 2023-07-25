7087be0b1baa073cf3122a0b632735eb333d8c91
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-07-25 10:04:07 +0300

    Динамические стили

--- src/components/Diagram/DiagramBar.js
+++ src/components/Diagram/DiagramBar.js
@@ -1,5 +1,25 @@
 import "./DiagramBar.css";
 
-const DiagramBar = (props) => {};
+const DiagramBar = (props) => {
+  let barFillHeight = "0%";
+
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
+  return (
+    <div className="diagram-bar">
+      <div className="diagram-bar__inner">
+        <div
+          className="diagram-bar__fill"
+          style={{
+            height: barFillHeight,
+          }}
+        ></div>
+        <span className="diagram-bar__label">{props.label}</span>
+      </div>
+    </div>
+  );
+};
 
 export default DiagramBar;
